6b11d4d8d64de13a6184aece14734eb3839fb4ec
zhouyulin123 <2321895324@qq.com>
2024-08-06 16:53:05 +0800

    high_quality_zh_clean

--- datasets/high_quality-diagnosis-and-treatment/iter1/high_quality_zh_clean.py
+++ datasets/high_quality-diagnosis-and-treatment/iter1/high_quality_zh_clean.py
@@ -37,91 +37,12 @@ class speicalProces:
     def step3_reference(self, context):
         new_context = []
         references_started = False   #定义一个删除reference的开关  只要出现固定格式的表述就对后面的内容进行删除
-        introduce = 0
-        introduce_index = []
-        Inc = 0
-        Inc_index = []
-
-        guidelines = 0
-        guidelines_index = []
-
-        MedicationGuide = 0
-        MedicationGuide_index = []
         for index, item in enumerate(context):
             if re.search(r'^(志 谢|References|参考文献|见参考文献|致谢|REFERENCES|ACKNOWLEDGMENT|The following organizations also provide reliable health information|More on this topic|Topic[^\.]*Version|For country code abbreviations|ACKNOWLEGMENT|WHERE TO GET MORE INFORMATION)', item.strip()):
                 references_started = True
             if references_started:
                 item = ""
-
-            if re.search(r'^2024© UpToDate, Inc', item.strip()):
-                Inc += 1
-                Inc_index.append(index)
-            if re.search(r'ALERT: ', item.strip()):
-                Inc -= 1
-                Inc_index.append(index)
-
-            # 要删除从Author到引言 设定了两个条件在循环时同时出现Author和引言，记下index，对相应的index进行删除
-            if re.search(r'^(Author)', item.strip()):
-                introduce += 1
-                introduce_index.append(index)
-            if re.search(r'^(引言|简介)', item.strip()) or re.search(r'^INTRODUCTION', item.strip()) or re.search(r'^Please read the Disclaimer at the end of this page',item.strip()):
-                introduce -= 1
-                introduce_index.append(index)
-
-            if re.search(r'^SOCIETY GUIDELINE LINKS',item.strip()):
-                guidelines += 1
-                guidelines_index.append(index)
-            if re.search(r'^INFORMATION FOR PATIENT',item.strip()) and guidelines == 0:
-                guidelines += 1
-                guidelines_index.append(index)
-            if re.search(r'^(.{0,5})(Basics topic|Beyond the Basics topic)', item.strip()):
-                guidelines -= 1
-                guidelines_index.append(index)
-
-            if re.search(r'^Medication Guide and/or',item.strip()):
-                MedicationGuide += 1
-                MedicationGuide_index.append(index)
-            if re.search(r'^Use: Labeled Indications',item.strip()):
-                MedicationGuide -= 1
-                MedicationGuide_index.append(index)
-
             new_context.append(item)
-
-        if introduce <= 0 and len(introduce_index) >= 2:
-            start_index = introduce_index[0]
-            end_index = introduce_index[-1]
-            # 循环遍历需要替换的片段
-            for i in range(start_index, end_index + 1):
-                # 将当前索引处的字符替换为你想要的字符，这里以空字符为例
-                new_context[i] = ''
-
-
-        if Inc <= 0 and len(Inc_index) >= 2:
-            start_index = Inc_index[0]
-            end_index = Inc_index[-1]
-            # 循环遍历需要替换的片段
-            for i in range(start_index, end_index + 1):
-                # 将当前索引处的字符替换为你想要的字符，这里以空字符为例
-                new_context[i] = ''
-
-
-
-        if guidelines <= 0 and len(guidelines_index) >= 2:
-            start_index = guidelines_index[0]
-            end_index = guidelines_index[-1]
-            # 循环遍历需要替换的片段
-            for i in range(start_index, end_index + 1):
-                # 将当前索引处的字符替换为你想要的字符，这里以空字符为例
-                new_context[i] = ''
-
-        if MedicationGuide <= 0 and len(MedicationGuide_index) >= 2:
-            start_index = MedicationGuide_index[0]
-            end_index = MedicationGuide_index[-1]
-            # 循环遍历需要替换的片段
-            for i in range(start_index, end_index):
-                # 将当前索引处的字符替换为你想要的字符，这里以空字符为例
-                new_context[i] = ''
-
         return new_context
     def step4_rm_kongge(self, context):
         context = context.lstrip().rstrip()
