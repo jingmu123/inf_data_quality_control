e59d2ad8f43c270481619eabb4ed8ef41c31612f
slimsu <363718368@qq.com>
2024-08-22 14:12:47 +0800

    update

---
+++ datasets/other-medlive_zh/iter2/clean_zh.py
@@ -0,0 +1,198 @@
+# coding:utf-8
+# https://github.com/shibing624/pycorrector?tab=readme-ov-file
+import json
+import re
+from collections import defaultdict
+
+from tqdm import tqdm
+import sys
+
+import transformers
+sys.path.append("../../../")
+
+import spacy
+import jieba
+
+# import torch.nn.functional as F
+# from transformers import AutoModelForSequenceClassification, AutoTokenizer
+# f7f8c3e1-218e-4792-92e4-5b096bfaa2c0
+
+pattern_list_zh = [
+    # [r'(（?\[([\u4e00-\u9fa5A-Za-z]+)<sup><img><\/sup>\]\(https?:\/\/[a-zA-Z0-9-]+\/\d*\)([\u4e00-\u9fa5A-Za-z]*)）?)',r'\2\3 删除1：<u>\1</u>'],
+    [r'(([(（])?\[([^\n[\]]*?)<sup><img><\/sup>\]\(https?:\/\/[a-zA-Z0-9-]+\/\d*\))',r'\2\3删除1：<u>\1</u>'],#多余链接和格式的删除
+
+    [r'(?:^[  ]*|\n[  ]*)((\*\*)?△*来[源自][:：].*(\*\*)?)',r'删除2：<u>\1</u>'],#来源类的整行删除
+    [r'(?:^[  ]*|\n[  ]*)((\*\*)?△*(截?图表?片?)[来源自略  ]+[:：]?.*(\*\*)?)',r'删除20：<u>\1</u>'],#图源类的删除整行删除
+    [r'(?:\n|^)([  ]*\**[图表] *[A-Za-z0-9一二三四五六七八九十:：]+.*\**)(?=\n|$)', r'删除6：<u>\1</u>'],  # 单独一行的图表索引
+    [r'((\*\*)?[(（] *(?:(?:详|流程)?见|配|如|视频|网络|截)*[图表] *[A-Za-z0-9一二三四五六七八九十]*[^(（）)\n]*?[）)](\*\*)?)', r'删除7：<u>\1</u>'],  # 文中的图表索引
+    [r'(?<=\n)(△?.{0,8}(?:资料|[截配制])图.{0,10})(?=\n|）|\))',r'删除26：<u>\1</u>'],#各式截图类的单行删除
+    [r'(?:。| |）|，|:)([^ 。,，\n(（）)]*?[如见]?下?[图表] *(?:[A-Za-z0-9一二三四五六七八九十]|所示)+[^。,，\n(（）)]*)(?:。|\n)',r'删除14：<u>\1</u>'],#文中出现的有关见图见表类的小句子的删除
+
+
+    [r'(?:\n|^)(\**[(（]?(第一作者|(本文)?编辑|(本文)?作者|撰稿人|资料图|联系.{0,3}|.{0,3}日期|.{0,3}邮箱|电话|手机|审批.{0,3}|致谢|支?持?单位|点评专家)[：:│].*\**)',r'删除8：<u>\1</u>'],
+    [r'(?<=\n)( * *责编 *[:：]?.*)|(?<=^)( * *责编 *[:：]?.*)',r'删除9：<u>\1\2</u>'],#单独一行的责编删除
+
+    [r'(<sup>(<span>)? *\[?.*?\]? *(<\/span>)?<\/sup>)',r'删除3：<u>\1</u>'],
+    [r'(\\?[\[［(][0-9０-９ ]+(?:[-—–－,，、][0-9０-９ ]+)*\\?[)］\]])',r'删除5：<u>\1</u>'],#文中的数字索引：\[5\]，\[16-17\]，\[7,8\]，［９］
+    [r'(?:\n|^)(\**(?:\\\*|…|参考文献 *略。?|↓|▼|摄图网|\**.*请勿转载.*\**|大会主持|。|致谢和参考文献（略）|.*内容仅限.*|.*以上观点系.*|本[期文]案例来自.*)+)(?=\n|$)',r'删除10：<u>\1</u>'],#单独一行\*或...的删除
+    [r'(?<=\n)((\*\*)?(参考文献(（向下滑动）)?[:：\n]?|参考来源[：:]|参考资料[：:]?|资料(?:来源|索引)[：:]|医脉通(?:编译)?整?理?自[：:]|原?文献索?引?[：:]|文源[：:]|医脉通综合整理|(论文|相关摘要)链接[：:]|以上内容摘自[:：]|今天你印象最深刻的新闻是？|.{0,4}互动[:：]|转载自[:：]|信源[:：]|[(（]?未完待续[)）]?)(\*\*)? *(([^\n]{3,})|(\n.*)*))',r'删除13：<u>\1</u>'],#文末互动： 今日互动：
+
+    [r'(?:\n|^)( *\**(?:（?摘要号 *[:：]|(?:英文|中文|原文?)?标题 *[:：]|文献链接 *[:：]|文章来源 *[:：]|延伸阅读 *[:：]|.{0,8}摘自\**[：:]|.{0,8}选自|.{0,8}整理自).*\**)',r'删除4：<u>\1</u>'],#ee1a37d2-a04e-4e8a-80ca-5a16de7af2d6
+
+
+
+
+
+
+
+    [r'(?<=^)(\**完整版指南下载链接.*)(?=\n|$)|(?<=\n)(\**完整版指南下载链接.*)(?=\n|$)',r'删除11：<u>\1\2</u>'],#单行的完整版指南下载 删除
+    [r'(\**[（(][^(（）)\n]*(?:链接|摘自|附件|改编|.化名)[^(（\n]*[）)] *(\*|\n\*)*)',r'删除12：<u>\1</u>'],#括号内的指南下载 删除
+    #6530a851-7287-43a1-80c8-e2ec518b668d
+
+    [r'(?<=\n)((\**（?(?:[^\n（]*：|[^\n（]{0,8}点击.*|.*专题报道.*|.*敬请关注.*|.*往期.*|.*回顾.*|.{0,10}推荐.*|✎)?[\n ]*(?:>>>|☞| |\*|↓|| |☟|✎|\d\.)*(?:\n *)*\**(?:(\[.*\])?　*\(?(?:https?:\/\/)(?:www\.)?[a-zA-Z0-9-]+(?:\.[a-zA-Z]{2,})+(?:\/[^ \n\u4e00-\u9fa5]*)?\)?)(?:》?）)?)+.*)',r'删除15：<u>\1</u>'],#后面有链接，标题符合的删除
+    [r'(.*(点击|戳|扫码).*?看.*?视频.*)|(.*看.*?视频.*?(点击|戳|扫码).*)',r'删除16：<u>\1\3</u>'],
+    [r'(以上为.*?的简要概括。.*)',r'删除17：<u>\1</u>'],
+    [r'([《（，\u4e00-\u9fa5“])(\[(.*)\](\((?:https?:\/\/)?(?:www\.)?[a-zA-Z0-9-]+(?:\.[a-zA-Z]{2,})+(?:\/[^ \n\u4e00-\u9fa5]*)?\)))',r'\1\3删除18：<u>\4</u>'],#提取书名或报道名（正文）
+    # [r'([(（][^\n()（）\\]*?参见[^\n()（）\\]*?[）)])',r'删除参见内容：<u>\1</u>']
+    [r'(\**（[^(（）)\n]*扫描?(?:下方|上方|文末)?(?:二维)?码[^（()）\n]*）\**)|([^(（\n]*(?:（.*）)?[^(（\n]*扫描?(?:下方|上方|文末)?(?:二维)码[^)）\n]*(?:（?.*）)?[^)）\n]*)',r'删除19：<u>\1\2</u>'],
+    [r'(\[(?:https?:\/\/)(?:www\.)?[a-zA-Z0-9-]+(?:\.[a-zA-Z]{2,})+(?:\/[^ \n\u4e00-\u9fa5]*)\]\(.*参考文献：.*\))',r'删除32：<u>\1</u>'],
+    [r'(【生产企业】(?:.|\n)*?)(?:\n *\n *\n|$)',r'删除21：<u>\1</u>'],
+    [r'(?<=\n)(\\\[[\u4e00-\u9fa5]+\\\] *)(?=\n)(\n\n)(?!\d)',r'删除22：<u>\1</u>\2'],
+    [r'([\（(][^\n(（]*?网站[^\n）)]*?[\）)])',r'删除23：<u>\1</u>'],
+
+    [r'(?<=\n)((.{4,7})(?:(?:(?:作|进行).{0,4}报告)|(开场致辞)))',r'删除24：<u>\1</u>'],
+    [r'(?<=\n)([A-Za-z]+号 *[A-Za-z0-9]+ *有效期 *\d{4}-\d{2}-\d{2}.*(\n\n)?(资料过期，视同作废。)?)',r'删除25：<u>\1</u>'],
+    [r'(?<=\n)((?:\**.*专家组\**)\n*\( *按姓氏笔画排序 *\)(?:.|\n)*?)(?:\n *\n *\n|$)',r'删除27：<u>\1</u>'],
+    [r'(.*欢迎(评论区)?留言.*)',r'删除28：<u>\1</u>'],
+    [r'(?<=)((?:[\u4e00-\u9fa5]{2,3} )+译)(?:\n)',r'删除29：<u>\1</u>'],
+    [r'([(（][A-Za-z .]+\d{4}[,，;]\d+(?:\(\d+\))?[:：,，]\d+[-–—]\d+[^\u4e00-\u9fa5\n]+[)）])',r'删除30：<u>\1</u>'],
+
+    #特殊case
+    [r'(\**(?:(?:欲知后事如何，请听下周日分解！|今天的医疗圈|发生了哪些与你有关的大事？|更新、更全的医学动态|更新、更全的医学动态|3分钟一网打尽|新旧版本更换期)\n*)+\**)',r'删除31：<u>\1</u>']
+
+
+
+
+]
+
+context_pattern = [
+    [r"[。，?!,]([。，?!,])", r"\1"],
+
+]
+
+
+# ner_pipeline = pipeline(Tasks.named_entity_recognition, 'damo/nlp_raner_named-entity-recognition_chinese-base-generic')
+class speicalProces:
+    def __init__(self):
+        pass
+        # self.sc = sentence_correct.SentenceCorrector()
+
+        # self.en_nlp = spacy.load("zh_core_web_trf")
+        # self.zh_nlp = spacy.load("zh_core_web_sm")
+        # self.zh_nlp = spacy.load("zh_core_web_sm")
+        # self.ner_pipeline = pipeline(Tasks.named_entity_recognition, 'damo/nlp_raner_named-entity-recognition_chinese-base-generic')
+        # self.zeroshot_classifier = transformers.pipeline("zero-shot-classification", model="deberta-v3-large-zeroshot-v2.0")
+        #
+        # self.model = AutoModelForSequenceClassification.from_pretrained("autonlp-Gibberish-Detector-492513457",
+        #                                                            use_auth_token=True)
+        # self.tokenizer = AutoTokenizer.from_pretrained("autonlp-Gibberish-Detector-492513457", use_auth_token=True)
+
+    # def step1_rm_reference(self,context):
+    #     if re.findall(r'((?:　)*(?: *  )+\d{4}年\d+月\d+日)',context) != 0:
+    #         if '附件' in context or '公告' in context:
+    #             return '本页判断为公告或附件，整页删除\n\n'+context
+    #     else:
+    #         return context
+    #
+    # def step2_Characte_introduction(self,context):
+    #     if re.findall(r'(\*\*.*[  ](副?教授|博士|主任)\*\*)(?=\n)((\n\n)*(.{3,51}))+',context) !=0:
+    #         pass
+    #     else:
+    #         return context
+
+    def step1_Irrelevant_long_text(self,context):
+        if re.findall(r'(?<=\n)(\**.*名单\**)\n*\(按姓氏笔画排序\)',context) !=[]:
+            return '本页判断为公告，整页删除\n\n'+context
+        else:
+            return context
+
+
+
+def clean_text(context, lang, sp):
+    # print("-"*10)
+    split_token = "\n\n"
+    if split_token not in context:
+        split_token = "\n"
+    pattern_list = pattern_list_zh
+
+
+    for pattern_item in pattern_list:
+        # context = context.lstrip(" ").rstrip(" ")
+        context = re.sub(pattern_item[0], pattern_item[1], context)
+    # context=sp.step1_rm_reference(context)
+    # 分解处理
+    # result = []
+    # for idx, item in enumerate(context.split(split_token)):
+    #     print(item)
+    #
+    #
+    #     for pattern_item in pattern_list:
+    #         item = item.lstrip(" ").rstrip(" ")
+    #         item = re.sub(pattern_item[0], pattern_item[1], item)
+    #
+    #     for replace_item in context_pattern:
+    #         item = re.sub(replace_item[0], replace_item[1], item)
+    #     print(item)
+    #     result.append(item)
+
+    # 整合
+
+    # print(context)
+    # context = split_token.join(result)
+    context=sp.step1_Irrelevant_long_text(context)
+    print(context)
+
+    return context
+
+
+def post_process(context):
+    context = context.strip(" ").strip("\n").strip(" ").strip("\n")
+    # 消除分界符失效  --*- 前面需要有连续两个\n;
+    context = re.sub('\n    --', "\n\n    --", context)
+    # 消除空格问题
+    context = re.sub(r'\n +\n', "\n\n", context)
+    context = re.sub(r'\n +\n', "\n\n", context)
+    # 去掉过多\n的情况
+    context = re.sub("\n{2,}", "\n\n", context)
+    return context
+
+
+def process_line(items, sp):
+    try:
+        item = json.loads(items.strip())
+        # item=sp.step1_drop_Pagefooter(item)
+
+        context = item["text"]
+        lang = item["lang"]
+
+        context = clean_text(context, lang, sp)
+        context = post_process(context)
+        item["text"] = context
+    except:
+        print("error")
+        exit(0)
+    item = json.dumps(item, ensure_ascii=False)
+    return item
+
+sp=speicalProces()
+fw = open(r'C:\Users\Administrator\PycharmProjects\untitled\other-medlive_zh_preformat\reclean2_other-medlive_zh_label.jsonl', 'a', encoding='utf-8')
+# with open(r'C:\Users\Administrator\PycharmProjects\untitled\other-medlive_zh_preformat\other-medlive_zh_preformat.jsonl', "r", encoding="utf-8") as file:
+with open(r'C:\Users\Administrator\PycharmProjects\untitled\other-medlive_zh_preformat\test.jsonl', "r", encoding="utf-8") as file:
+
+    for item in tqdm(file.readlines()):
+
+        item=process_line(item,sp)
+        # with open(r'F:\zeroshots\reclean7_medicalpdfv2__preformat_zh.jsonl', 'a', encoding='utf-8') as f:
+        fw.write(item+'\n')
+
+
