0cdfae7c8aecf465ba3cc626a065b8ee32588657
zhouyulin123 <2321895324@qq.com>
2024-09-04 16:32:47 +0800

    common_re

--- clean_tools/common_re.py
+++ clean_tools/common_re.py
@@ -11,36 +11,46 @@ class clean_pattern:
             # 固定格式  带有（）的图片表格描述 附录描述 协议描述   顺序不能打乱
             [r'(\(\s?([Ff]igs?(ure)?|F\s?IGS?(URE)?|Table|[Ss]ee|For more|panel|http|www|NCT\d+|NO\.|version|p\.)s?[\s\.:][^\(\)]*\))',r''],   # 1. 这些固定的词语紧贴左括号
             [r'(\(\s?[^\(\)]*)([\.;]\s([Ff]igs?(ure)?|F\s?IGS?(URE)?|Table|[sS]ee|For more|http|www)s?[\s\.:][^\(\)]*)(\))',r'\1)'],   # 这些固定的词语在句子中间但是前半句可能有用 用[\.;]\s来判断前半句是否结束
-            [r'(\([^\(\)]*([Ff]igs?(ure)?|F\s?IGS?(URE)?|Table|[sS]ee\s|For more|http|www|NCT\d+|N[oO]\.|Participant \d+|Provider \d+|software|version)s?[\s\.:][^\(\)]*\))',r'通用删除1(英):<u>\1</u>'], # 最广泛的形式从左括号匹配到右括号
+            [r'(\([^\(\)]*([Ff]igs?(ure)?|F\s?IGS?(URE)?|Table|[sS]ee\s|For more|http|www|NCT\d+|N[oO]\.|Participant \d+|Provider \d+|software|version)s?[\s\.:][^\(\)]*\))',r''], # 最广泛的形式从左括号匹配到右括号
 
-            [r'(.*,\s?et[\s\xa0]{1,3}al.*)', r'通用删除2(英):<u>\1</u>'],     # , et al   et al一版在一些人名后面，一定要加逗号，如果没有逗号可能会造成一些误删
+            [r'(.*,\s?et[\s\xa0]{1,3}al.*)', r''],     # , et al   et al一版在一些人名后面，一定要加逗号，如果没有逗号可能会造成一些误删
             [r'^\b(\w+(\s\w+){0,})\s+(\1)\b', r'\1'],  # 解决句首出现的单词重复的问题
-            [r'(^[\*#]{0,4}(NEWSLETTER|Get the Crohn|Tips from experts|Stay Up-to-Date|Sign up for the latest coronavirus news|You can find more information at|See also|Adapted by|For more information).*)',r'通用删除3(英):<u>\1</u>'], # 开头固定这种情况较多这种固定开头后面都能添加 (时事通讯|获取克罗恩资讯|专家提示|了解最新动态|注册获取最新冠状病毒新闻|你可以寻找更多消息在...|另请参见...|改编自...|更多信息)
-            [r'(?<![\dm\s])(\s{0,}<sup>(<a>)?[\d\s\–—,\(\)\[\]]{1,20}(</a>)?</sup>)', r'通用删除4(英):<u>\1</u>'],     # 特殊数字  排除可能出现的次幂情况
-            [r'(.*(doi|DOI)\s?:.*)', r'通用删除5(英):<u>\1</u>'],  # 存在有DOI描述的句子
-            [r'((\\)?\[[\d\s,\-\–—]{1,}(\\)?\])', r'通用删除6(英):<u>\1</u>'],  # 带有方括号的数字引用
-            [r'((\\)?\([\d\s,\-\–—]{1,}(\\)?\))', r'通用删除7(英):<u>\1</u>'],  # 带有圆括号的数字引用
-            [r'((\\)?\[\s?[^\[\]]*([Ff]igs?(ure)?|F\s?IGS?(URE)?|Table|[sS]ee|For more|panel|http|www|NCT\d+|NO\.|version)s?[^\[\]]*(\\)?\])',r'通用删除8(英):<u>\1</u>'],   # 固定格式  带有[]的图片表格描述 附录描述 协议描述 无关网址描述
-            [r'(^Full size.*)', r'通用删除9(英):<u>\1</u>'],  # Full size image/table 原文这里应该是一个图/表没识别出图形
+            [r'(^[\*#]{0,4}(NEWSLETTER|Get the Crohn|Tips from experts|Stay Up-to-Date|Sign up for the latest coronavirus news|You can find more information at|See also|Adapted by|For more information).*)',r''], # 开头固定这种情况较多这种固定开头后面都能添加 (时事通讯|获取克罗恩资讯|专家提示|了解最新动态|注册获取最新冠状病毒新闻|你可以寻找更多消息在...|另请参见...|改编自...|更多信息)
+            [r'(?<![\dm\s])(\s{0,}<sup>(<a>)?[\d\s\–—,\(\)\[\]]{1,20}(</a>)?</sup>)', r''],     # 特殊数字  排除可能出现的次幂情况
+            [r'(.*(doi|DOI)\s?:.*)', r''],  # 存在有DOI描述的句子
+            [r'((\\)?\[[\d\s,\-\–—]{1,}(\\)?\])', r''],  # 带有方括号的数字引用
+            [r'((\\)?\([\d\s,\-\–—]{1,}(\\)?\))', r''],  # 带有圆括号的数字引用
+            [r'((\\)?\[\s?[^\[\]]*([Ff]igs?(ure)?|F\s?IGS?(URE)?|Table|[sS]ee|For more|panel|http|www|NCT\d+|NO\.|version)s?[^\[\]]*(\\)?\])',r''],   # 固定格式  带有[]的图片表格描述 附录描述 协议描述 无关网址描述
+            [r'(^Full size.*)', r''],  # Full size image/table 原文这里应该是一个图/表没识别出图形
             [r'(\([^\(\)]*(arrow|←|→)[^\(\)]\))', r''],  # ...箭头 描述图里面不同颜色的箭头
 
+            [r'(^Full size.*)', r'通用删除10(英):<u>\1</u>'],  # Full size image/table 原文这里应该是一个图/表没识别出图形
+            [r'(\([pP]\.?\d+[^\(\)]*\))', r'通用删除11(英):<u>\1</u>'],     # 带有括号的([Pp]. ...)第几页
+            [r'(\([^\(\)]*Additional file[^\(\)]*\))', r''],              # 附加文件带括号
+            [r'([\*#]{0,4}Additional file.*)', r'通用删除12(英):<u>\1</u>'],      # 附加文件
+            [r'(^Download\s.*)', r'通用删除13(英):<u>\1</u>'],  # 段落开头下载 ...
         ]
         return pattern_en
 
     # 通用中文正则
     def clean_pattern_zh(self):
         pattern_zh = [
-            [r'([\(][^\)\(]*见?(图|表|详见)\s?\d+[^\)\(]*[\)])', r'通用删除1(中):<u>\1</u>'],  # 带有英文括号的
-            [r'(（[^）（]*见?(图|表|详见)\s?\d+[^）（]*）)', r'通用删除2(中):<u>\1</u>'],
-            [r'(致谢.*)',r'通用删除3(中):<u>\1</u>'],
-            [r'(^[\*#]{0,4}点击查看.*)',r'通用删除4(中):<u>\1</u>'],     # 点击查看...
+            [r'([\(][^\)\(]*见?(图|表|详见)\s?\d+[^\)\(]*[\)])', r''],  # 带有英文括号的
+            [r'(（[^）（]*见?(图|表|详见)\s?\d+[^）（]*）)', r''],
+            [r'(致谢.*)',r''],
+            [r'(^[\*#]{0,4}点击查看.*)',r''],     # 点击查看...
             # [r'(^[\*#]{0,4}(图|表)\s?\d+$)',r'通用删除5(中):<u>\1</u>'],   # 这一段中只有一个 表\d+
-            [r'(.*利益冲突.*)',r'通用删除6(中):<u>\1</u>'],    # 文章末利益冲突
-            [r'(^[\*#]{0,4}详见.*)',r'通用删除7(中):<u>\1</u>'],    # 详见...
-            [r'(^[\*#]{0,4}阅读更多.*)',r'通用删除8(中):<u>\1</u>'],  # 阅读更多...
-            [r'((\\)?\[[\d\s,\-\–—]{1,}(\\)?\])', r'通用删除9(中):<u>\1</u>'],  # 带有方括号的数字引用
+            [r'(.*利益冲突.*)',r''],    # 文章末利益冲突
+            [r'(^[\*#]{0,4}详见.*)',r''],    # 详见...
+            [r'(^[\*#]{0,4}阅读更多.*)',r''],  # 阅读更多...
+            [r'((\\)?\[[\d\s,\-\–—]{1,}(\\)?\])', r''],  # 带有方括号的数字引用
             # [r'((\\)?\([\d\s,\-\–—]{1,}(\\)?\))', r'通用删除10(中):<u>\1</u>'],  # 带有圆括号的数字引用
-            [r'(?<![\dm\s])(\s{0,}<sup>(<a>)?[\d\s\–—,\(\)\[\]]{1,20}(</a>)?</sup>)', r'通用删除11(中):<u>\1</u>'],  # 特殊数字  排除可能出现的次幂情况
+            [r'(?<![\dm\s])(\s{0,}<sup>(<a>)?[\d\s\–—,\(\)\[\]]{1,20}(</a>)?</sup>)', r''],  # 特殊数字  排除可能出现的次幂情况
+
+            [r'(（\s{0,}）)', r'通用删除12(中):<u>\1</u>'],  # 空括号里面什么都没有
+            [r'(（详见[^（）]*）)', r'通用删除13(中):<u>\1</u>'],  # 中文括号详见...
+            [r'([，。]见(图|表)\d+[^，。]*)', r'通用删除14(中):<u>\1</u>'],  # 半句到前后的标点处截至 见图/表1...
+            [r'(（[^（）]*视频[^（）]*）)', r'通用删除15(中):<u>\1</u>'],  # 带有中文（）的...视频
         ]
         return pattern_zh
 
@@ -71,8 +81,8 @@ class clean_pattern:
                 end_index = index
         if end_index > 0:
             for i in range(0, end_index + is_add):
-                context[i] = "通用开头删除-1:<u>{}</u>".format(context[i])
-                # context[i] = ""
+                # context[i] = "通用开头删除-1:<u>{}</u>".format(context[i])
+                context[i] = ""
         return context
 
     # 通用删除从某一个段开始到文章结束
@@ -89,8 +99,8 @@ class clean_pattern:
             if re.search(start, item.strip()):
                 references_started = True
             if references_started:
-                item = "通用结尾删除-1:<u>{}</u>".format(item)
-                # item = ''
+                # item = "通用结尾删除-1:<u>{}</u>".format(item)
+                item = ''
             new_context.append(item)
         return new_context
 
@@ -117,6 +127,6 @@ class clean_pattern:
             start_index = delete_line_index[0]
             end_index = delete_line_index[-1]
             for i in range(start_index, end_index + is_add):
-                context[i] = "通用间距删除-1:<u>{}</u>".format(context[i])
-                # context[i] = ""
+                # context[i] = "通用间距删除-1:<u>{}</u>".format(context[i])
+                context[i] = ""
         return context
